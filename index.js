ee648c504ecf7d960afab421f786a2ef04eedfb4
kAtsukinAritA <64456240+kAtsukinAritA@users.noreply.github.com>
2022-07-07 01:46:24 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.querySelectorAll("img").forEach((imgElm) => {
 
  //モーダル表示
  $(".modal-open").modaal({
-    start_open:flag, // ページロード時に表示するか
+    //start_open:flag, // ページロード時に表示するか
     overlay_close:true,//モーダル背景クリック時に閉じるか
     before_open:function(){// モーダルが開く前に行う動作
       $('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
